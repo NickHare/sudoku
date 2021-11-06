9000cc2cc52abd727041a85342f60f977b2fe9ed
Nick <cs.NickHare@gmail.com>
2021-11-05 22:00:28 -0400

    Separated Classes.

--- sudoku_position.py
+++ sudoku_position.py
@@ -9,6 +9,34 @@ class SudokuPosition:
         self.row_num = row_num
         self.col_num = col_num
 
+    @staticmethod
+    def get_row_positions(row_num):
+        assert 0 <= row_num <= SudokuBoard.ROW_SIZE
+        values = []
+        for c in range(SudokuBoard.COL_SIZE):
+            values.append = (row_num, c)
+        return values
+
+    @staticmethod
+    def get_col_positions(col_num):
+        assert 0 <= col_num <= SudokuBoard.COL_SIZE
+        values = []
+        for r in range(SudokuBoard.ROW_SIZE):
+            values.append = (r, col_num)
+        return values
+
+    @staticmethod
+    def get_box_positions(box_num):
+        assert 0 <= box_num <= SudokuBoard.SIZE
+
+        row_start = (box_num // SudokuBoard.BOX_SIZE) * SudokuBoard.BOX_SIZE
+        col_start = (box_num % SudokuBoard.BOX_SIZE) * SudokuBoard.BOX_SIZE
+        values = []
+        for r in range(SudokuBoard.BOX_SIZE):
+            for c in range(SudokuBoard.BOX_SIZE):
+                values.append = (row_start + r, col_start + c)
+        return values
+
     def __eq__(self, other):
         return self.row_num == other.row_num and self.col_num == other.col_num
 
---
+++ sudoku_position_test.py
@@ -0,0 +1,6 @@
+import pytest
+
+from sudoku_board import SudokuBoard
+from sudoku_board import SudokuPosition
+
+# def
