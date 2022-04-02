8f256e4dba21de18e3ec8a4edf56792a41e34836
Nick <cs.NickHare@gmail.com>
2022-04-01 21:26:01 -0400

    Added start to client.

---
+++ sudoku-client/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Sudoku',
+            filename: 'index.html',
+            template: './index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                ],
+            },
+        ]
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        watchFiles: ['index.html'],
+        compress: true,
+        port: 9000,
+    },
+}
